d9532a612468651d8e3adc3018f95da724021e8c
Beatriz G <bea_gonci@hotmail.es>
2024-10-21 00:06:23 -0500

    Visualizacion de los Partidos y Candidatos

--- src/app/services/party.service.ts
+++ src/app/services/party.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +9,29 @@ export class PartyService {
   private selectedPartySubject = new BehaviorSubject<any>(null);
   selectedParty$ = this.selectedPartySubject.asObservable();
 
+  constructor(private http: HttpClient) {}
+
   setParty(party: any) {
-    this.selectedPartySubject.next(party); // Emite el nuevo partido seleccionado
+    this.selectedPartySubject.next(party);
   }
+  
 
   getParty() {
-    return this.selectedPartySubject.getValue(); // Devuelve el partido actual
+    return this.selectedPartySubject.getValue();
   }
+
+  // Método para obtener partidos y candidatos desde el backend
+  getParties(): Observable<any> {
+    const url = 'http://localhost:8080/api/partidos'; // Cambia esta URL a la de tu backend
+    return this.http.get<any>(url);
+  }
+
+  // party.service.ts
+    getSelectedPartyId(): number | null {
+      const party = this.selectedPartySubject.getValue();
+      return party ? party.idPartido : null;
+    }
+
+
+  
 }
