c0d856dc8fe03f264d17c5d97ff076452fc2771a
Beatriz G <bea_gonci@hotmail.es>
2024-11-04 22:08:45 -0500

    Verificacion del Reconocimiento Facial

--- src/app/folder/voto/verificacion/verificacion.page.ts
+++ src/app/folder/voto/verificacion/verificacion.page.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { UserModalComponent } from 'src/app/user-modal/user-modal.component';  // Adjust the path if necessary
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-verificacion',
   templateUrl: './verificacion.page.html',
   styleUrls: ['./verificacion.page.scss'],
 })
-export class VerificacionPage implements OnInit {
+export class VerificacionPage {
   videoUrl: string = 'http://127.0.0.1:5000/video_feed'; // URL del backend Flask
-  loadingIcon: boolean = false;  // Indicador para mostrar el ícono de carga
 
-  constructor(private router: Router) {}
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ){
     this.iniciarVerificacion();
   }
 
-  iniciarVerificacion() {
-   
-    setTimeout(() => {
-      this.loadingIcon = true; 
-      setTimeout(() => {
-        this.router.navigate(['/folder/Inbox/voto/categorias']); 
-      }, 2000);
-    }, 30000);
+  iniciarVerificacion(){
+    const verificarUsuario = setInterval(() => {
+      this.http.get('http://127.0.0.1:5000/usuario_recibido')
+        .subscribe(
+          (respuesta : any) => {
+            console.log(respuesta)
+            if(respuesta) {
+              clearInterval(verificarUsuario);
+              this.recibirDatos(respuesta);
+            }
+          },
+          error => {
+            console.error("Error al obtener los datos del usuario: ", error);
+          }
+        )
+    })
   }
+
+  recibirDatos(usuario: any) {
+    if (usuario) {
+      const { id_usuario, nombre, apellidos } = usuario;  // Usa la clave adecuada
+
+      console.log(`Usuario encontrado: ID: ${id_usuario}, Nombre: ${nombre} ${apellidos}`);
+
+      // Cambiar la ruta
+      this.router.navigate(['/folder/Inbox/voto/categorias']);
+    } else {
+      console.log("No se encontró el usuario.");
+    }
+  }
 }
